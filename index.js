df7b57f7752d0cc8ec61ecef3d72cb8984ce90e4
Seth <seth5124@Gmail.com>
2021-08-03 18:42:50 -0400

    removed some comments

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import * as game from './game.js';
 import * as display from './display.js';
 import * as AI from './AI.js';
-//TODO: Fix extra move bug
+
 AI.initializeAI();
 display.initializeBoard();
 display.initializePlayerDisplay();
