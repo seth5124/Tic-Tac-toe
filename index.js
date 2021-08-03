bc702b975a87d59000d1fcb9f2e556a3ef31b0e5
Seth <seth5124@gmail.com>
2021-08-03 15:16:41 -0400

    changed main.js to index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+import * as game from './game.js';
+import * as display from './display.js';
+import * as AI from './AI.js';
+//TODO: Fix extra move bug
+AI.initializeAI();
+display.initializeBoard();
+display.initializePlayerDisplay();
+
+
+document.addEventListener('reset', ()=>{
+    game.reset();
+    display.updateBoard();
+    display.resetActivePlayerMarker();
+})
