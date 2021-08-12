acf2a11d6014db0f503b0cad4aee50504d08df4a
Seth Bone <seth5124@gmail.com>
2021-08-12 16:56:58 -0400

    Minification set up. Prepared for merge to master

--- webpack.common.js
+++ webpack.common.js
@@ -1,18 +1,21 @@
 const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
     entry: "./src/index.js",
-    output: {
-      assetModuleFilename: "assets/imgs/[name].[hash].[ext]",
-    },
-    plugins: [new HtmlWebpackPlugin({
+
+    plugins: [
+      new HtmlWebpackPlugin({
       template: "./src/template.html"
-    })],
+    }),
+      new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css'
+      })],
     module: {
       rules: [
         {
           test: /\.css$/i,
-          use: ["style-loader", "css-loader"],
+          use: [MiniCssExtractPlugin.loader, "css-loader"],
         },
         {
           test: /\.html$/i,
@@ -21,12 +24,15 @@ module.exports = {
         {
           test:/\.(svg|png)$/i,
           type:'asset/resource',
+          generator: {
+            filename: 'assets/imgs/[name].[contenthash].[ext]'
+          }
         },
         {
           test:/\.otf$/i,
           type:'asset/resource',
           generator: {
-            filename: 'assets/fonts/[name].[hash].ext'
+            filename: 'assets/fonts/[name].[hash].[ext]'
           },
 
         }
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,19 @@
 const path = require('path');
 const common = require('./webpack.common.js')
 const {merge} = require('webpack-merge');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
 module.exports = merge(common, {
-  mode: "development",
+  mode: "production",
     output:{
         filename: "app.[contenthash].js",
         path: path.resolve(__dirname, 'DIST'),
         clean: true,
+    },
+    optimization:{
+        minimizer: [new CssMinimizerPlugin(),
+                    new TerserPlugin()],
     },
 
 });
